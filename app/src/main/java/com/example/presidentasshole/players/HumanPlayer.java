53662c411ffb57b4de1b496d76644ebd32c8b8e1
Claire <porterc24@up.edu>
2022-03-29 11:54:30 -0700

    Added gameframework and changes imports

--- app/src/main/java/com/example/presidentasshole/players/HumanPlayer.java
+++
@@ -1,9 +0,0 @@
-package com.example.presidentasshole.players;
-
-import com.example.presidentasshole.PresidentGame;
-
-public class HumanPlayer extends Player {
-    public HumanPlayer(PresidentGame game) {
-        super(game);
-    }
-}
